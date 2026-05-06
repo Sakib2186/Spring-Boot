387c13d797273ee5687dd07d3f880f475a2930f7
Sakib2186 <skmdsakib2186@gmail.com>
2026-05-06 22:51:09 +0600

    feat: add validation class for request bodies onl

--- src/main/java/com/eazybytes/jobportal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eazybytes/jobportal/exception/GlobalExceptionHandler.java
@@ -3,11 +3,16 @@ package com.eazybytes.jobportal.exception;
 import com.eazybytes.jobportal.Dto.ExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,6 +24,15 @@ public class GlobalExceptionHandler {
                 exception.getMessage(), LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDto,HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+    // Dto Validation exception method
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleException(MethodArgumentNotValidException exception){
+        Map<String,String> errors = new HashMap<>();
+        List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
+        fieldErrorList.forEach(error->errors.put(error.getField(),error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors); // 400 Status code
 
     }
 
