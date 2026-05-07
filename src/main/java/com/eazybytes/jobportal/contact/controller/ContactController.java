06de7b36d8add8aeabab67067673db3cf22075b9
Sakib2186 <skmdsakib2186@gmail.com>
2026-05-07 20:18:36 +0600

    Learned about Params validation using @Validation and creating own custom exception

--- src/main/java/com/eazybytes/jobportal/contact/controller/ContactController.java
+++ src/main/java/com/eazybytes/jobportal/contact/controller/ContactController.java
@@ -3,9 +3,12 @@ package com.eazybytes.jobportal.contact.controller;
 import com.eazybytes.jobportal.Dto.ContactRequestDto;
 import com.eazybytes.jobportal.contact.controller.service.IContactService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +26,13 @@ public class ContactController {
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error sending contact request");
 
+    }
+
+    @GetMapping(version = "1.0",path="/")
+    public ResponseEntity<String> fetchOpenContacts(@RequestParam @Validated
+                                                        @NotBlank(message="Status is required")
+                                                        @Size(min=4,message = "Status length should be of minimum 4 chars")
+                                                        String status){
+        return ResponseEntity.ok().body("Status: "+status);
     }
 }
--- src/main/java/com/eazybytes/jobportal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eazybytes/jobportal/exception/GlobalExceptionHandler.java
@@ -4,15 +4,18 @@ import com.eazybytes.jobportal.Dto.ExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -33,6 +36,24 @@ public class GlobalExceptionHandler {
         List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
         fieldErrorList.forEach(error->errors.put(error.getField(),error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors); // 400 Status code
+
+    }
+
+    // Params Validation Error Exception
+    // Also used for PathVariable Validation Exception
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<Map<String,String>> handleException(HandlerMethodValidationException exception){
+        Map<String,String> errors = new HashMap<>();
+        List<ParameterValidationResult> results = exception.getParameterValidationResults();
+        results.forEach(result->{
+            String paramName = result.getMethodParameter().getParameterName();
+
+            String combinedMessages = result.getResolvableErrors()
+                    .stream()
+                    .map(error->error.getDefaultMessage())
+                    .collect(Collectors.joining(","));
+            errors.put(paramName,combinedMessages);
+        });
 
     }
 
